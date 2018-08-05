7e2e20b94e659bf067c7dff11413c0fc564fc5a5
Matthew Muccio <me@matthewmuccio.com>
2018-08-05 03:21:06 -0400

    Adds challenge code for Day 8.

---
+++ day8.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+
+
+if __name__ == "__main__":
+    n = int(input())
+    name_numbers = [input().split() for _ in range(n)]
+    phone_book = {k: v for k,v in name_numbers}
+
+    while True:
+        try:
+            name = input()
+            if name in phone_book:
+                print('%s=%s' % (name, phone_book[name]))
+            else:
+                print('Not found')
+        except:
+            break
