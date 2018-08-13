c0dcefb08c70a3cfcdd12e28784e07485e65c752
Matthew Muccio <me@matthewmuccio.com>
2018-08-13 16:11:42 -0700

    Adds challenge code for Day 15.

---
+++ day15.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+
+class Solution:
+    def display(self,head):
+        current = head
+        while current:
+            print(current.data,end=' ')
+            current = current.next
+
+    def insert(self, head, data):
+        if (head == None):
+            head = Node(data)
+        elif (head.next == None):
+            head.next = Node(data)
+        else:
+            self.insert(head.next, data)
+        return head
+
+
+if __name__ == "__main__":
+	mylist = Solution()
+	T=int(input())
+	head=None
+	for i in range(T):
+		data=int(input())
+		head=mylist.insert(head,data)
+	mylist.display(head);
