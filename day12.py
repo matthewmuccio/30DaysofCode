7f527eeb600f79d0d0600d50f49ea4883420aa40
Matthew Muccio <me@matthewmuccio.com>
2018-08-18 14:33:34 -0400

    Adds challenge code for Day 12.

--- day12.py
+++ day12.py
@@ -1,38 +1,40 @@
 #!/usr/bin/env python3
 
 
-class Student(Person):
-    #   Class Constructor
-    #
-    #   Parameters:
-    #   firstName - A string denoting the Person's first name.
-    #   lastName - A string denoting the Person's last name.
-    #   id - An integer denoting the Person's ID number.
-    #   scores - An array of integers denoting the Person's test scores.
-    #
-    # Write your constructor here
-    def __init__(self, firstName, lastName, idNumber, scores):
-        self.firstName = firstName
-        self.lastName = lastName
-        self.idNumber = idNumber
-        self.scores = scores
+class Person:
+	def __init__(self, firstName, lastName, idNumber):
+		self.firstName = firstName
+		self.lastName = lastName
+		self.idNumber = idNumber
+	def printPerson(self):
+		print("Name:", self.lastName + ",", self.firstName)
+		print("ID:", self.idNumber)
 
-    #   Function Name: calculate
-    #   Return: A character denoting the grade.
-    #
-    # Write your function here
+
+import builtins
+
+def print(*args,**kwargs):
+    if "sep" not in kwargs and args[0] =="Grade: ":
+        builtins.print(*args,**kwargs,sep="")
+    else:
+        builtins.print(*args,**kwargs)
+
+class Student(Person):
+    def __init__(self, firstName, lastName, idNumber, scores):
+        super().__init__(firstName, lastName, idNumber)
+        self.scores = scores
     def calculate(self):
-        score_sum = sum([score for score in self.scores])
-        grade = score_sum/len(self.scores)
-        if 90 <= grade <= 100:
-            return "O"
-        elif 80 <= grade < 90:
-            return "E"
-        elif 70 <= grade < 80:
-            return "A"
-        elif 55 <= grade < 70:
-            return "P"
-        elif 40 <= grade < 55:
-            return "D"
-        else:
-            return "T"
+        iAvg = sum(self.scores)/len(self.scores)
+        return 'O' if iAvg > 89 else 'E' if iAvg > 79 else 'A' if iAvg > 69 else 'P' if iAvg > 54 else 'D' if iAvg > 39 else 'T'
+
+
+if __name__ == "__main__":
+    line = input().split()
+    firstName = line[0]
+    lastName = line[1]
+    idNum = line[2]
+    numScores = int(input())
+    scores = list( map(int, input().split()) )
+    s = Student(firstName, lastName, idNum, scores)
+    s.printPerson()
+    print("Grade: ", s.calculate())
