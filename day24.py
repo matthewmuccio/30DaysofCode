70f5c64986f7a318f9954a6e7298e2193ebd4bc4
Matthew Muccio <me@matthewmuccio.com>
2018-08-25 00:01:47 -0400

    Refactors challenge code for Day 24.

--- day24.py
+++ day24.py
@@ -2,26 +2,26 @@
 
 
 class Node:
-    def __init__(self,data):
+    def __init__(self, data):
         self.data = data
         self.next = None
 class Solution:
-    def insert(self,head,data):
+    def insert(self, head, data):
             p = Node(data)
-            if head==None:
-                head=p
-            elif head.next==None:
-                head.next=p
+            if head == None:
+                head = p
+            elif head.next == None:
+                head.next = p
             else:
-                start=head
-                while(start.next!=None):
-                    start=start.next
-                start.next=p
+                start = head
+                while (start.next != None):
+                    start = start.next
+                start.next = p
             return head
-    def display(self,head):
+    def display(self, head):
         current = head
         while current:
-            print(current.data,end=' ')
+            print(current.data, end=' ')
             current = current.next
     def removeDuplicates(self, head):
         current = head
